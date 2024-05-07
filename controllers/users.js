a545c84a309758117b9c4b9e680ae9e6de289423
[Eli Harel] <[eliharel3@gmail.com]>
2024-05-06 19:52:56 -0600

    Controllers - Added controllers for campgrounds, reviews, users - Tidied router files with router.route() - Added star icons for reviews rather than a slider

---
+++ controllers/users.js
@@ -0,0 +1,44 @@
+const User = require('../models/user');
+
+module.exports.renderLogin = (req, res) => {
+    res.render('users/login');
+};
+
+module.exports.register = async (req, res, err) => {
+    try {
+
+        const { username, email, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome to YelpCamp!');
+            res.redirect('/campgrounds');
+        })
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('/register');
+    }
+};
+
+module.exports.renderRegister = (req, res) => {
+    res.render('users/register');
+};
+
+module.exports.login = (req, res) => {
+    req.flash('succuess', 'Welcome back!');
+    const redirectURL = res.locals.returnTo || '/campgrounds';
+    res.redirect(redirectURL);
+};
+
+module.exports.logout = (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', 'Goodbye!');
+        res.redirect('/campgrounds');
+
+    });
+};
--- routes/reviews.js
+++ routes/reviews.js
@@ -3,26 +3,12 @@ const router = express.Router({ mergeParams: true });
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 const Campground = require('../models/campground');
 const Review = require('../models/review');
+const reviews = require('../controllers/reviews.js');
 const ExpressError = require('../utils/ExpressError');
 const catchAsync = require('../utils/catchAsync');
 
-router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    campground.reviews.push(review);
-    await campground.save();
-    await review.save();
-    req.flash('success', 'Review added');
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview));
 
-router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash('success', 'Review deleted');
-    res.redirect(`/campgrounds/${id}`);
-}))
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview));
 
 module.exports = router;
